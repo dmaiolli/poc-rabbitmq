5d3b43b0070be0aa01720cc602434dcf941441c1
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2022-01-18 21:05:12 -0300

    First setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # poc-rabbitmq
+
+RUN Rabbit
+docker run -d -p 5672:5672 -p 15672:15672 --name=rabbitmq rabbitmq:3.8.3-management
+
---
+++ producer/src/test/java/br/com/denys/poc/rabbit/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.denys.poc.rabbit.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
